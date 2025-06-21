2a820e86ee70c22a094068f1a9b870084142cded
Kithir Ajmal <kithir.ajmal@flyerssoft.com>
2025-06-21 13:31:20 +0530

    added the  config

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your Firebase config — get this from Firebase Console > Project settings > General > SDK setup and config
 const firebaseConfig = {
-  apiKey: "AIzaSyBk4foRN-em0jkSnBGqUleQPwqBjVhj580",
-  authDomain: "kag-form.firebaseapp.com",
-  projectId: "kag-form",
-  storageBucket: "kag-form.firebasestorage.app",
-  messagingSenderId: "100908221290",
-  appId: "1:100908221290:web:633956157a9968045f9b13",
-  measurementId: "G-XCS7N41XY0"
+  apiKey: "AIzaSyDslfaD4KSCc3uQ5WEO88fFj6T9sRPBrWQ",
+  authDomain: "kag-tiles-54095.firebaseapp.com",
+  projectId: "kag-tiles-54095",
+  storageBucket: "kag-tiles-54095.firebasestorage.app",
+  messagingSenderId: "997098427550",
+  appId: "1:997098427550:web:11ab2a757162a4beb4178f",
+  measurementId: "G-3THDHH246L"
 };
 
 // Initialize Firebase
